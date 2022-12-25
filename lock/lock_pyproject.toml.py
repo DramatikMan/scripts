820427a219192ed89b9b0abd1a408c57704a5161
Sergey Pavlov <self@sergeypavlov.dev>
2022-12-25 13:44:21 +0300

    Update pyproject.toml lock script

--- lock/lock_pyproject.toml.py
+++ lock/lock_pyproject.toml.py
@@ -10,19 +10,25 @@ if __name__ == "__main__":
     regex_dep = re.compile(r'^(\S*).*=\s"(\*)"')
     regex_req = re.compile(r"^(\S*)==(\S*)")
     req_map = {}
+    exact = False
+
+    if len(sys.argv) > 3 and sys.argv[3] == "--exact":
+        exact = True
 
     with open(sys.argv[2]) as reqs:
         for line in reqs.readlines():
-            req = re.match(regex_req, line)
+            req = re.match(regex_req, line.lower())
 
             if req:
                 req_map[req.group(1)] = req.group(2)
 
+    prefix = "" if exact else "^"
+
     with open(sys.argv[1]) as deps:
         for line in deps.readlines():
-            dep = re.match(regex_dep, line)
+            dep = re.match(regex_dep, line.lower())
 
             if dep:
-                line = line.replace("*", req_map[dep.group(1)])
+                line = line.replace("*", prefix + req_map[dep.group(1)])
 
             print(line, end="")
