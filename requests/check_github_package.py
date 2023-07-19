360a78f05acfbc24b1305a83a53085aec04fdb97
Sergey Pavlov <self@sergeypavlov.dev>
2023-07-19 13:16:03 +0300

    Update scripts environment

--- requests/check_github_package.py
+++ requests/check_github_package.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 """
 GHCR_PAT env var = GitHub PAT with read permission for packages
 first arg = repo_name as in https://github.com/{user_name}/{repo_name}
@@ -6,23 +7,20 @@ first arg = repo_name as in https://github.com/{user_name}/{repo_name}
 import os
 import sys
 
-import requests
+import urllib3
 
 if __name__ == "__main__":
-    token = os.environ["GHCR_PAT"]
-    repo = sys.argv[1]
-    url = f"https://api.github.com/user/packages/container/{repo}/versions"
-
-    resp = requests.get(
-        url=url,
+    resp: urllib3.BaseHTTPResponse = urllib3.request(
+        "GET",
+        f"https://api.github.com/user/packages/container/{sys.argv[1]}/versions",
         headers={
-            "Authorization": f"token {token}",
+            "Authorization": f"token {os.environ['GHCR_PAT']}",
             "Accept": "application/vnd.github.v3+json",
         },
     )
 
-    print(f"IMAGE_EXISTS={1 if resp.status_code == 200 else 0}")
+    print(f"IMAGE_EXISTS={1 if resp.status == 200 else 0}")
 
-    if resp.status_code == 200:
+    if resp.status == 200:
         latest_id = resp.json()[0]["id"]
         print(f"LATEST_IMAGE_ID={latest_id}")
