c9ffe0a4bf66924d8ce57779df4cd512e94d61a6
Sergey Pavlov <self@sergeypavlov.dev>
2023-08-04 21:51:12 +0300

    Add pubspec.yaml lock script

---
+++ lock/pubspec_yaml.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+"""
+first arg = path to pubspec.yaml
+second arg = path to output of "dart pub" command
+"""
+
+import re
+import sys
+from tempfile import TemporaryFile
+
+import yaml
+
+if __name__ == "__main__":
+    regex_req = re.compile(r"── (?P<name>[\@\d\w\/\-\_]+)\s{1}(?P<version>[\d\.]+)")
+    req_map: dict[str, str] = {}
+    exact = False
+
+    if len(sys.argv) > 3 and sys.argv[3] == "--exact":
+        exact = True
+
+    with open(sys.argv[2]) as actual:
+        first_line = actual.readline()
+        SDK = first_line[:-1].split(" ")[-1]
+
+        for line in actual.readlines():
+            req = re.search(regex_req, line)
+
+            if req is not None:
+                req_map[req.group("name")] = req.group("version")
+
+    prefix = "" if exact else "^"
+
+    with open(sys.argv[1]) as pubspec_yaml:
+        deps = yaml.load(pubspec_yaml, Loader=yaml.Loader)
+
+        for target in ("dependencies", "dev_dependencies"):
+            if (category := deps.get(target)) is None:
+                continue
+
+            for key in category:
+                category[key] = prefix + req_map[key]
+
+        deps["environment"]["sdk"] = f"{prefix}{SDK}"
+
+    with TemporaryFile(mode="w+") as temp:
+        yaml.dump(deps, temp, Dumper=yaml.Dumper, indent=2, sort_keys=False)
+        temp.seek(0)
+
+        for line in temp.readlines():
+            if (
+                line.startswith("environment")
+                or line.startswith("dependencies")
+                or line.startswith("dev_dependencies")
+            ):
+                print(f"\n{line}", end="")
+                continue
+
+            print(line, end="")
---
+++ lock/test/test_pubspec_yaml.py
@@ -0,0 +1,37 @@
+import subprocess as sp
+from pathlib import Path
+
+import pytest
+import yaml
+
+cwd = Path(__file__).parent
+script_path = cwd.parent.joinpath("pubspec_yaml.py").absolute()
+pubspec_yaml_in_path = cwd.joinpath("asset/in.pubspec.yaml").absolute()
+dart_pub_deps_in_path = cwd.joinpath("asset/in.dart.pub.deps.txt").absolute()
+
+
+@pytest.mark.parametrize(
+    ("arg", "expected_result_path"),
+    (
+        ("", cwd.joinpath("asset/out.pubspec.yaml").absolute()),
+        ("--exact", cwd.joinpath("asset/out.exact.pubspec.yaml").absolute()),
+    ),
+)
+def test_package_json_pnpm(arg: str, expected_result_path: Path) -> None:
+    rv = sp.run(
+        [
+            "python3",
+            str(script_path),
+            str(pubspec_yaml_in_path),
+            str(dart_pub_deps_in_path),
+            arg,
+        ],
+        capture_output=True,
+    )
+
+    result = yaml.load(rv.stdout, Loader=yaml.Loader)
+
+    with open(expected_result_path, "rb") as handle:
+        expected_result = yaml.load(handle, Loader=yaml.Loader)
+
+    assert result == expected_result
